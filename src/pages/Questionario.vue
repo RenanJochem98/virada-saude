aa4872f7fffc430a251fb0f89294436bba28e837
RenanJochem98 <renanjochem98@gmail.com>
2022-03-28 11:03:15 -0300

    Adicionando validação ao questionário

--- src/pages/Questionario.vue
+++ src/pages/Questionario.vue
@@ -3,16 +3,21 @@
     <q-page class="">
         <div class="text-center text-h4 text-weight-bolder q-mt-lg">Questionario inicial</div>
         <div class="text-center text-weight-bolder q-mt-sm">Breve descrição sobre o questionario</div>
+        
         <div class="row flex flex-center q-mt-lg q-pt-lg">
-            <div v-for="(index, i) in [6,5,4,3,2,1]" :key="i" class="col-12 text-center q-py-lg">
-                <div class="text-weight-medium">{{i + 1}} - Enunciado da questão</div>
-                <div v-for="(jIndex, j) in [6,5,4,3,2,1]" :key="j">
-                    <q-radio v-model="shape" :val="j" :label="'Opção: ' + jIndex" />
+            <div v-for="(index, i) in questions" :key="i" class="col-12 q-mt-lg">
+                <div class="text-center text-weight-medium">{{i + 1}} - Enunciado da questão</div>
+                
+                <div v-if="clicouEmEnviar && (respostas[index] == null || respostas[index] === undefined)" class="text-negative text-center text-caption q-mt-xs q-mb-sm">Campo obrigatório</div>
+
+                <div class="q-mt-sm col-12">
+                    <q-option-group :ref="questionRefPrefix + index" v-model="respostas[index]" :options="options"  class="flex justify-center items-center"
+                    @update:model-value="respostas[index] == $event"/>
                 </div>
             </div>
         </div>
         <div class="row flex flex-center">
-            <q-btn label="Continuar" class="btn-continuar q-mb-sm col-5" :to="{ name: 'Home' }" />
+            <q-btn label="Continuar" class="btn-continuar q-mb-sm col-5" @click="sendAnswers"  />
         </div>
     </q-page>
 </template>
@@ -25,12 +30,49 @@
 <script>
 import { defineComponent } from "vue";
 
+const options = [
+    {
+        label: 'Option 1',
+        value: 'op1'
+    },
+    {
+        label: 'Option 2',
+        value: 'op2'
+    },
+    {
+        label: 'Option 3',
+        value: 'op3'
+    },
+]
+
 export default defineComponent({
   name: "Questionario",
   data: function() {
     return {
-        shape: ""
+        options,
+        shape: "",
+        respostas: {},
+        questions: [6,5,4],
+        clicouEmEnviar: false,
+        questionRefPrefix: "questionRef"
     };
-}
+  },
+  methods: {
+      sendAnswers () {
+        this.clicouEmEnviar = true
+        let temErro = false
+        
+        temErro = this.questions.length !== Object.values(this.respostas).filter(i =>  i !== null && i !== undefined && i !== '').length
+
+        //   this.$refs.fieldRef1[0].validate()
+        if(!temErro){
+            console.log('passou') 
+            this.$router.push( { name: 'Home' })
+        }else {
+            console.log('nao passou')
+        }
+        //   
+      }
+  }
 });
 </script>
