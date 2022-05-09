0b1e0186650e75d258dbd11d8bdb2b6cbe8b6524
RenanJochem98 <renanjochem98@gmail.com>
2022-05-08 22:23:05 -0300

    Adicionando app para usuarios

---
+++ api/core/users/api/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer representando os dados de um usuário
+    """
+
+    # token = serializers.CharField(source="auth_token.key", read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "username"]
---
+++ api/core/users/models.py
@@ -0,0 +1,81 @@
+# from datetime import datetime
+# import constants
+
+# from hashids import Hashids
+
+from django.utils import timezone
+from django.contrib import admin
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+# from django.core.exceptions import PermissionDenied
+from django.db import models
+
+# from exams.models import Exam
+
+
+# hashids = Hashids(min_length=3, alphabet="abcdefghijklmnopqrstuvwxyz0123456789")
+
+
+# class RegistrationForm(models.Model):
+#     name = models.CharField(max_length=255)
+
+#     def __str__(self) -> str:
+#         return self.name
+
+#     def encode(self) -> str:
+#         return hashids.encode(self.id)
+
+#     @classmethod
+#     def decode(cls, string: str) -> "RegistrationForm":
+#         return cls.objects.filter(id=hashids.decode(string)[0]).first()
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create(self, **kwargs):
+        return super().create(**kwargs)
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password=password, **extra_fields)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+# class User(models.Model):
+    email = models.EmailField("Email", blank=True, null=False)
+    username = models.CharField("username", max_length=150, blank=False, null=False)
+    first_name = models.CharField("first_name", max_length=150, blank=True, null=True)
+    last_name = models.CharField("last_name", max_length=150, blank=True, null=True)
+    is_active = models.BooleanField("is_active", default=True)
+    is_staff = models.BooleanField("is_staff", default=False)
+    is_superuser = models.BooleanField("is_superuser", default=False)
+
+    date_joined = models.DateTimeField("date_joined", default=timezone.now)
+    created = models.DateTimeField("created", default=timezone.now)
+    modified = models.DateTimeField("modified", auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["email"],
+                name="unique_email",
+            )
+        ]
+
+    def __str__(self):
+        return self.email
