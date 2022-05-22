5710b49e90a7e516e6a9991df2027ba0f8cb7251
RenanJochem98 <renanjochem98@gmail.com>
2022-05-22 18:54:45 -0300

    Melhornado serialização do usuário

--- api/core/users/api/serializers.py
+++ api/core/users/api/serializers.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
+from empresa.api.serializers import EmpresaSerializer
 
 User = get_user_model()
 
@@ -12,9 +13,13 @@ class UserSerializer(serializers.ModelSerializer):
     Serializer representando os dados de um usuário
     """
 
-    token = serializers.CharField(source="auth_token.key", read_only=True)
-
+    # token = serializers.CharField(source="auth_token.key", read_only=True)
+    empresa = EmpresaSerializer(many=False, read_only=True)
     class Meta:
         model = User
-        fields = ["id", "email", "username", "token"]
-        read_only_fields = ["token"]
+        fields = ["id", "email", "first_name", "last_name", "created", "modified", "empresa"]
+        read_only_fields = ["created", "modified"]
+        # write_only_fields = ["password"]
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
