db642b8dac13077513c94a5b80990566bc8f57f0
RenanJochem98 <renanjochem98@gmail.com>
2022-05-22 18:58:08 -0300

    Alterando serializer para permitir criação de senha.

--- api/core/users/api/serializers.py
+++ api/core/users/api/serializers.py
@@ -17,9 +17,11 @@ class UserSerializer(serializers.ModelSerializer):
     empresa = EmpresaSerializer(many=False, read_only=True)
     class Meta:
         model = User
-        fields = ["id", "email", "first_name", "last_name", "created", "modified", "empresa"]
+        fields = ["id", "email", "password", "first_name", "last_name", "created", "modified", "empresa"]
         read_only_fields = ["created", "modified"]
-        # write_only_fields = ["password"]
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
