02191a38921a45e81db3e52022b5916e2f987394
RenanJochem98 <renanjochem98@gmail.com>
2022-07-18 17:51:33 -0300

    Adicionando funções para exibir processamento do axios.

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,6 +1,6 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
-
+import { Notify, Loading } from 'quasar'
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
 // If any client changes this (global) instance, it might be a
@@ -8,6 +8,34 @@ import axios from "axios";
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({ baseURL: "http://localhost:8000/api" });
+function showCustom () {
+  Notify.create({
+    type: 'warning',
+    message: 'Processando requisição',
+    spinner: true,
+    timeout: 1000
+  })
+}
+api.interceptors.request.use(function (request) {
+  showCustom()
+  Loading.show({
+    message: '<b>Processando requisição</b>',
+    html: true,
+    backgroundColor: 'grey-10'
+  })
+  return request
+}, function (error) {
+  Loading.hide()
+  return Promise.reject(error)
+})
+
+api.interceptors.response.use(function (response) {
+  Loading.hide()
+  return response
+}, function (error) {
+  Loading.hide()
+  return Promise.reject(error)
+})
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
