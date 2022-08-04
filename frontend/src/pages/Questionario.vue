5049119ff313d628178a973225449ca9ddaa7c47
RenanJochem98 <renanjochem98@gmail.com>
2022-08-03 22:54:46 -0300

    Altera questionario para exibir questoes dependentes dinamicamente.

--- frontend/src/pages/Questionario.vue
+++ frontend/src/pages/Questionario.vue
@@ -5,16 +5,14 @@
         <div class="text-center text-weight-bolder q-mt-sm">Breve descrição sobre o questionario</div>
         
         <div class="row flex flex-center q-mt-lg q-pt-lg">
-            <div v-for="(pergunta, i) in perguntas" :key="i" class="col-12 q-mt-lg">
-                <div v-if="pergunta.dependeDe == null">
-                    <div class="text-center text-weight-medium">{{i + 1}} - {{pergunta.texto}}</div>
-                
-                    <div v-if="clicouEmEnviar && (respostas[pergunta.idPerguntaAnamnese] == null || respostas[pergunta.idPerguntaAnamnese] === undefined)" class="text-negative text-center text-caption q-mt-xs q-mb-sm">Campo obrigatório</div>
+            <div v-for="(pergunta, i) in perguntas" :key="i" :class="pergunta.dependeDe !== null ? 'hidden' : '' + 'row flex-center col-12 questao q-py-lg'" :id="questionRefPrefix + pergunta.idPerguntaAnamnese">
+                <div class="text-center text-weight-medium">{{i + 1}} - {{pergunta.texto}}</div>
+            
+                <div v-if="clicouEmEnviar && (respostas[pergunta.idPerguntaAnamnese] == null || respostas[pergunta.idPerguntaAnamnese] === undefined)" class="text-negative text-center text-caption q-mt-xs q-mb-sm">Campo obrigatório</div>
 
-                    <div class="q-mt-sm col-12">
-                        <q-option-group :ref="questionRefPrefix + pergunta.idPerguntaAnamnese" v-model="respostas[pergunta.idPerguntaAnamnese]" :options="pergunta.opcoesResposta"  class="flex justify-center items-center"
-                        @update:model-value="respostas[pergunta.idPerguntaAnamnese] == $event"/>
-                    </div>
+                <div class="q-mt-sm col-12">
+                    <q-option-group :ref="questionRefPrefix + pergunta.idPerguntaAnamnese" v-model="respostas[pergunta.idPerguntaAnamnese]" :options="pergunta.opcoesResposta"  class="flex justify-center items-center"
+                    @update:model-value="alterarRespostas(pergunta.idPerguntaAnamnese, $event)"/>
                 </div>
             </div>
         </div>
@@ -33,18 +31,6 @@
 import { defineComponent } from "vue";
 import { PerguntaAnamnseController } from '../services/controllers/PerguntasAnamneseController'
 const options = [
-    {
-        label: 'Option 1',
-        value: 'op1'
-    },
-    {
-        label: 'Option 2',
-        value: 'op2'
-    },
-    {
-        label: 'Option 3',
-        value: 'op3'
-    },
 ]
 
 export default defineComponent({
@@ -76,21 +62,51 @@ export default defineComponent({
         } else {
             this.perguntas = perguntas
         }
-        console.log('questionario options', this.perguntas)
+        // console.log('questionario options', this.perguntas)
+      },
+      alterarRespostas (idQuestao, idResposta) {
+        this.respostas[idQuestao] == idResposta
+        // console.log("Perguntas: ", this.perguntas)
+        // console.log("Respostas: ", this.respostas)
+        // console.log("Refs: ", this.$refs)
+        // console.log("Ref Quest 1: ",this.$refs.questionRef1)
+        //document.getElementById(this.questionRefPrefix + "1").classList.remove('hidden')
+
+        // Mostra questoes dependentes escondidas
+        let dependentesRespostas = this.perguntas.filter(p => {
+            return p.dependeDe && p.dependeDe.id_opcao_resposta_anamnese == idResposta
+        })
+        dependentesRespostas.forEach(d => {
+           document.getElementById(this.questionRefPrefix + d.idPerguntaAnamnese).classList.remove('hidden')
+        })
+
+        //Esconde questoes dependentes que ja foram exibidas, mas com tiveram respostas alteradas
+        let dependentesPerguntas = this.perguntas.filter(p => {
+            return p.dependeDe && p.dependeDe.id_pergunta_anamnese == idQuestao
+        })
+
+        dependentesPerguntas.forEach(d => {
+            if(this.respostas[idQuestao] && d.dependeDe.id_opcao_resposta_anamnese != this.respostas[idQuestao]) {
+                document.getElementById(this.questionRefPrefix + d.idPerguntaAnamnese).classList.add('hidden')
+            }
+           
+        })
       },
       sendAnswers () {
+        console.log("Respostas: ", this.respostas)
+        console.log("Refs: ", this.$refs)
         this.clicouEmEnviar = true
         let temErro = false
         
-        temErro = this.questions.length !== Object.values(this.respostas).filter(i =>  i !== null && i !== undefined && i !== '').length
+        // temErro = this.questions.length !== Object.values(this.respostas).filter(i =>  i !== null && i !== undefined && i !== '').length
 
         //   this.$refs.fieldRef1[0].validate()
-        if(!temErro){
-            console.log('passou') 
-            this.$router.push( { name: 'Home' })
-        }else {
-            console.log('nao passou')
-        }
+        // if(!temErro){
+        //     console.log('passou') 
+        //     this.$router.push( { name: 'Home' })
+        // }else {
+        //     console.log('nao passou')
+        // }
         //   
       }
   }
