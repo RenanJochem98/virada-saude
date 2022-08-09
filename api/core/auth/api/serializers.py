f6d5e8407e944ea0abdef38ebe78a343839e265a
RenanJochem98 <renanjochem98@gmail.com>
2022-08-08 22:06:17 -0300

    Adiciona informações de cadastro ao login

--- api/core/auth/api/serializers.py
+++ api/core/auth/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
+from anamnese.models import Anamnese
+from TempoDisponivel.models import TempoDisponivel
 # from anamnese.models import PerguntasAnamnese, OpcaoRespostaAnamnese, Anamnese 
 # from users.api.serializers import UserSerializer
 class AuthSerializer(TokenObtainPairSerializer):
@@ -11,5 +12,9 @@ class AuthSerializer(TokenObtainPairSerializer):
         # Add custom claims
         token['id_user'] = user.id
         token['email'] = user.email
+        token['possui_anamnese'] = len(Anamnese.objects.filter(usuario_id = user.id)) > 0
+        token['possui_tempo_disponivel_cadastrado'] =  len(TempoDisponivel.objects.filter(id_usuario = user.id)) > 0
+        #adicionar função para buscar se tem anamnese
+        #adicionar função para buscar se já adicionouo tempo disponivel
         return token
 
