8201971d405c64f13e28d09e6976882890653993
RenanJochem98 <renanjochem98@gmail.com>
2022-08-11 10:29:36 -0300

    Altera parametros de envio dad dados para API.

--- frontend/src/pages/TempoDisponivel.vue
+++ frontend/src/pages/TempoDisponivel.vue
@@ -1,22 +1,16 @@
 <template>
     <q-page padding>
         <div class="text-center text-h4 text-weight-bold q-ma-lg text-uppercase">Tempo Disponível</div>
-        <div v-for="i in quantLinhas" :key="i">
+        <div v-for="dia in diasSemana" :key="dia.value">
             <div class="row flex flex-center">
-                <div class="col-2 col-md-2 col-lg-2 q-px-lg q-mx-lg">
-                    <q-select v-model="listaPeriodos[i].dia" :options="diasSemana" label="Dia" />
+                <div class="col-xs-12 col-sm-12 col-1 col-md-1 col-lg-1 text-bold flex flex-center">
+                    {{dia.label}}
                 </div>
-                <div class="col-1 col-md-1 col-lg-1 q-mx-lg">
-                    <InputTime @changeTime="listaPeriodos[i].horaInicio = $event"/>
-                </div>
-                <div class="col-1 col-md-1 col-lg-1 q-mx-lg">
-                    <InputTime @changeTime="listaPeriodos[i].horaFim = $event"/>
-                </div>
+                <q-radio v-for="(periodo, index) in [30, 45, 60]" :key="index" 
+                    class="col-xs-12 col-sm-12 col-1 col-md-1 col-lg-1 flex flex-center" v-model="listaPeriodos[dia.value]" 
+                    :val="periodo" 
+                    :label="periodo + ' min'" />
             </div>
-        </div>
-        <div class="row flex flex-center">
-            <q-btn class="col-6 col-md-2 col-lg-6 q-px-lg q-mx-lg" color="green-9" icon-right="add_circle" label="Adicionar" @click="addLinha"/>
-            <q-btn class="col-6 col-md-2 col-lg-6 q-px-lg q-mx-lg" color="red-9" icon-right="remove_circle" label="Remover" @click="removeLinha"/>
         </div>
         <div class="row flex flex-center q-pt-lg q-mt-lg">
             <q-btn label="Continuar" class="btn-continuar q-mb-sm col-5" @click="enviar"  />
@@ -34,49 +28,22 @@
 import { defineComponent } from "vue";
 import { DiaSemanaController } from '../services/controllers/DiaSemanaController'
 import { TempoDisponivelController } from '../services/controllers/TempoDisponivelController'
-import InputTime  from 'src/components/Inputs/InputTime.vue'
 
 export default defineComponent({
   name: "TempoDisponivel",
-  components: { InputTime },
+//   components: { InputTime },
   data: () => {
       return {
         diasSemana: [],
-        quantLinhas: 1,
-        listaPeriodos: {
-            1: {
-                dia: "",
-                horaInicio: "",
-                horaFim: "",
-            }
-        }
+        listaPeriodos: []
       }
   },
   beforeMount(){
-    this.buscarPerguntas()
+    this.buscarDiasSemana()
   },
   methods: {
-      addLinha () {
-        this.quantLinhas++
-        this.listaPeriodos[this.quantLinhas] = {
-                dia: "",
-                horaInicio: "",
-                horaFim: "",
-            }
-      },
-      removeLinha () {
-        if(this.quantLinhas > 1) {
-            delete this.listaPeriodos[this.quantLinhas]
-            this.quantLinhas--
-        } else {
-            this.$q.notify({
-                type: 'warning',
-                message: "Você precisa adicionar pelo menos um período."
-            })
-        }
-        
-      },
-      async buscarPerguntas(){
+      
+      async buscarDiasSemana(){
         const diasSemana = await DiaSemanaController.BuscarDiasSemana()
         console.log("diasSemana", diasSemana)
         if(diasSemana.status) {
@@ -93,23 +60,17 @@ export default defineComponent({
       async enviar(){
         console.log("id user logado", this.$store.getters['user/getIdUser'])
         console.log("listaperiodos: ", this.listaPeriodos)
-
         let teste = []
-        const list = Object.values(this.listaPeriodos)
-        console.log("list", list)
-        list.forEach(e => {
-            if(e.dia) {
+        this.diasSemana.forEach(dia => {
+            if(this.listaPeriodos[dia.value]){
                 let item = {
-                "id_dia_semana": e.dia.value,
-                "id_usuario": this.$store.getters['user/getIdUser'],
-                "hora_inicio": e.horaInicio,
-                "hora_fim": e.horaFim
+                    "id_dia_semana": dia.value,
+                    "id_usuario": this.$store.getters['user/getIdUser'],
+                    "periodo_disponivel": this.listaPeriodos[dia.value],
+                }
+                teste.push(item)
             }
-            teste.push(item)
-            }
-            
-        });
-        console.log("teste", teste)
+        })
 
         // Deve ser alterado para um bulk create, quando a api aceitar
         await teste.forEach(e => {
