f05e649aa35415ba2161f964882ecaa49d1cf4d0
RenanJochem98 <renanjochem98@gmail.com>
2022-08-27 11:05:24 -0300

    Adiciona filtro por usuario na busca de periodos disponiveis.

--- api/core/TempoDisponivel/api/views.py
+++ api/core/TempoDisponivel/api/views.py
@@ -9,6 +9,7 @@
 from rest_framework.permissions import IsAuthenticated
 # from rest_framework.response import Response
 from rest_framework import  viewsets #mixins,
+# from rest_framework.filters import SearchFilter
 
 from TempoDisponivel.api.serializers import (
     TempoDisponivelSerializer, DiaSemanaSerializer
@@ -16,9 +17,22 @@ from TempoDisponivel.api.serializers import (
 from TempoDisponivel.models import TempoDisponivel, DiaSemana
 
 class TempoDisponivelViewSet(viewsets.ModelViewSet):
-    queryset = TempoDisponivel.objects.all()
+    # queryset = TempoDisponivel.objects.all()
     serializer_class = TempoDisponivelSerializer
     permission_classes = [IsAuthenticated]
+    # filter_backends = [ SearchFilter ]
+    # search_fields = ['id_usuario']
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        user = self.request.query_params.get('id_usuario')
+        if user is not None:
+            queryset = TempoDisponivel.objects.filter(id_usuario=user)
+        else:
+            queryset = TempoDisponivel.objects.all()
+        return queryset
 
 class DiaSemanaViewSet(viewsets.ModelViewSet):
     queryset = DiaSemana.objects.all()
