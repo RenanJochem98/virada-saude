b79d5d08d899e84ad08d5f8792e37afb5f08e431
RenanJochem98 <renanjochem98@gmail.com>
2022-09-17 12:03:03 -0300

    Altera forma de visualização do dia no tempo disponível.

--- api/core/TempoDisponivel/api/serializers.py
+++ api/core/TempoDisponivel/api/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 
 from TempoDisponivel.models import TempoDisponivel, DiaSemana
 
+class DiaSemanaSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DiaSemana
+        fields = ["id_dia_semana", "nome"]
+        
 class TempoDisponivelListSerializer(serializers.ListSerializer):
     def create(self, validated_data, many=True):
         books = [TempoDisponivel(**item) for item in validated_data]
@@ -9,6 +15,7 @@ class TempoDisponivelListSerializer(serializers.ListSerializer):
 
 class TempoDisponivelSerializer(serializers.ModelSerializer):
 
+    # id_dia_semana = DiaSemanaSerializer(many=False, read_only=True)
     class Meta:
         model = TempoDisponivel
         fields = ["id_tempo_disponivel", "id_dia_semana", "id_usuario", "periodo_disponivel"]
@@ -17,10 +24,11 @@ class TempoDisponivelSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data, many=True):
         return TempoDisponivel.objects.create(**validated_data)
+    
+    def to_representation(self, instance):
+        representation = super(TempoDisponivelSerializer, self).to_representation(instance)
+        representation['id_dia_semana'] = DiaSemanaSerializer(instance.id_dia_semana).data
+        return representation
 
-class DiaSemanaSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = DiaSemana
-        fields = ["id_dia_semana", "nome"]
     
