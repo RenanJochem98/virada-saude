e3ecf177d59e172736531f7ae370839cfc5ccebb
RenanJochem98 <renanjochem98@gmail.com>
2023-01-24 09:31:34 -0300

    Inicio de pwa.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,5 +6,15 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
+  beforeMount(){
+    this.teste()
+  },
+  methods: {
+    teste() {
+      console.log(process.env.URL_API)
+      console.log(process.env.URLA_API)
+      console.log(process.env)
+    }
+  }
 });
 </script>
